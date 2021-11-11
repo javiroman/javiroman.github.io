39e1fe6a14d4a146562ae6abdcbb247043e71406
Javi Roman <jroman.espinar@gmail.com>
2021-11-11 20:49:10 +0100

    fix typo

--- run-local.sh
+++ run-local.sh
@@ -1,3 +1,4 @@
-#  sudo dnf install rubygem-bundler -y
-# bundle install  --path vendor/bundle
+# 1. Install rvm: https://rvm.io
+# 2. rvm install "ruby-2.4.10"
+# 3. bundle install  --path vendor/bundle
 bundle exec jekyll serve
